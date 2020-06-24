4914716d504b29f3c8cae785100ea5a2a7337934
Brice VIGNAL <vignal.brice@gmail.com>
2020-06-24 11:54:16 +0200

    todo's app made in sveltejs framework

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Todos from "./components/Todos.svelte";
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <Todos />
+</main>
